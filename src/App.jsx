df3d3de1601ffd2bab42f13ddd531e1322be953e
Razieh <raziye_f@yahoo.com>
2025-03-04 13:43:37 +0100

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
+import { useState,useEffect} from 'react'
+import ShowDetail from "./components/ShowDetail"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Razi codes
+  const [modalOpen,setModalOpen] = useState(false);
+  const [diaryDetail,setDiaryDetail] = useState({
+    title: "",
+    date: "",
+    image:"",
+    content:""
+  });
+
+  const handleClickModal = (diaryDate) =>{
+    // setModalOpen(true); 
+  //   useEffect(() => {
+  //   const diaryList = JSON.parse(localStorage.getItem(diaries)) || [];
+  //   const selectedDiary = diaryList.filter(diary => diary.date == diaryDate);
+  //   setDiaryDetail(selectedDiary);
+  // }, [diaryDate]);
+  }
+  // razi codes
 
   return (
     <>
-    
+      {/* Razieh codes */}
+      <div>
+      <i className="fa-solid fa-circle-info cursor-pointer text-[#00514f] text-xl" 
+      onClick={() => setModalOpen(true)}></i>
+      </div>
+      <ShowDetail isOpen={modalOpen} setModalOpen={setModalOpen} diaryDetail={diaryDetail} setDiaryDetail={setDiaryDetail}/>
+      {/* Razieh code end */}
     </>
   )
 }

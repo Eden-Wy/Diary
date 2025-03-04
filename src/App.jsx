7d4ee9cbd3dba8dd6e7d424d1069e621586002e8
Abdulwahed Bin Mothana <abdulwahed.mothana@yahoo.com>
2025-03-04 11:02:21 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,7 @@ function App() {
    <h1 className="text-4xl text-blue-600 font-bold text-center mb-6" style={{ fontFamily: "'Bebas Neue', cursive" }}>
         Personal Diary
       </h1>
-      <Router>
-      <div className="min-h-screen bg-gray-100 p-5">
-        <Routes>
-          <Route path="/" element={<DisplayList />} />
-        
-        </Routes>
-      </div>
-    </Router>
+   <DisplayList />
 
     </>
   )

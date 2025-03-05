0c5bc7707a75c6be8428798b26ce00826fdde2a9
Razieh <raziye_f@yahoo.com>
2025-03-05 17:15:18 +0100

    add form and modal for new entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-
+import { useState } from "react";
 import DisplayList from "./components/DisplayList";
+import AddEntry from "./components/AddEntry";
 
 function App() {
   const entries = localStorage.getItem("diaryEntries") || [];
+  const [isAddModalOpen,setIsAddModalOpen] = useState(false);
 
   return (
     <>
       <h1 className="text-4xl text-[#005477] font-bold text-center py-5" style={{ fontFamily: "'Bebas Neue', cursive" }}>
         Personal Diary
       </h1>
+      <button className="absolute top-[25px] right-[25px] bg-[#005477] rounded-sm p-2 text-[#ccc]
+      cursor-pointer" onClick={()=> setIsAddModalOpen(true)}>
+        Add Entry
+      </button>
       <DisplayList />
+      <AddEntry isAddModalOpen={isAddModalOpen} setIsAddModalOpen={setIsAddModalOpen} />
     </>
   )
 }
--- src/components/AddEntry.jsx
+++ src/components/AddEntry.jsx
@@ -1,19 +1,19 @@
 // src/components/NewEntry.js
 import { useState, useEffect } from "react";
 
-const addEntry = () => {
-    const entry = newEntry();
-    <button onClick={addEntry}>Add Entry</button>
+// const addEntry = () => {
+//     const entry = newEntry();
+//     <button onClick={addEntry}>Add Entry</button>
 
 
-};
+// };
 
-function newEntry() {
-    const [entryFields, setEntryFields] = useState([
+function AddEntry({isAddModalOpen,setIsAddModalOpen}) {
+    const [newEntry, setNewEntry] = useState([
         {
             title: "",
             date: "",
-            imageURL: "",
+            image: "",
             content: ""
         }
     ]);
@@ -25,42 +25,92 @@ function newEntry() {
         });
       };
 
+    const submitForm = (e) =>{
+        e.preventDefault();
+        setNewEntry(
+            {
+                title: "",
+                date: "",
+                image: "",
+                content: ""
+            });
+    }
+
     return (
-    <div className="newEntry">
-      <form onSubmit={submit}>
-        {entryFields.map((entry, index) => {
-            return (
-                <div key={index}>
-                    <input
-                        name="Title"
-                        placeholder="Enter a title for today's entry"
-                        value={entry.title}
-                        onChange={handleChange}
-                    />
-                    <input
-                        name="date"
-                        placeholder='Enter a date for this entry'
-                        value={entry.date}
-                        onChange={handleChange}
-                    />
-                    <input
-                        name="imageURL"
-                        placeholder='Image URL'
-                        value={entry.imageURL}
-                        onChange={handleChange}
-                    />
-                    <input
-                        name="content"
-                        placeholder="Dear diary... "
-                        value={entry.content}
-                        onChange={handleChange}
-                    />
-                </div>
-            )
-        })}
-     </form>
-    </div>
-    );
+        <>
+            <div className= {`modal-container fixed top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2
+                 rounded-3xl border-2 shadow-md border-[#fff6e0] p-3 ${isAddModalOpen ? "opened-modal" : "closed-modal"}`}>
+            <div className="bg-[rgba(202,202,202,0.7)] w-full h-full max-h-full overflow-y-scroll p-3 rounded-2xl">
+                <i className="fas fa-times-circle text-red-800 absolute -top-[7px] -right-[7px]
+                text-3xl cursor-pointer" onClick={()=> setIsAddModalOpen(false)}></i>
+                <form onSubmit={submitForm}>
+                            <div key={newEntry.date}>
+                                 <input
+                                    name="Title"
+                                    placeholder="Enter a title for today's entry"
+                                    value={newEntry.title}
+                                    onChange={handleChange}
+                                />
+                                <input
+                                    name="date"
+                                    placeholder='Enter a date for this entry'
+                                    value={newEntry.date}
+                                    onChange={handleChange}
+                                />
+                                <input
+                                    name="image"
+                                    placeholder='Image'
+                                    value={newEntry.image}
+                                    onChange={handleChange}
+                                />
+                                <input
+                                    name="content"
+                                    placeholder="Dear diary... "
+                                    value={newEntry.content}
+                                    onChange={handleChange}
+                                />
+                                <button type="submit">Enter</button>
+                            </div>
+                 </form>
+            </div>
+      
+      </div>
+        </>
+    // <div className="newEntry">
+    //   <form onSubmit={submit}>
+    //     {entryFields.map((entry, index) => {
+    //         return (
+    //             <div key={index}>
+    //                 <input
+    //                     name="Title"
+    //                     placeholder="Enter a title for today's entry"
+    //                     value={entry.title}
+    //                     onChange={handleChange}
+    //                 />
+    //                 <input
+    //                     name="date"
+    //                     placeholder='Enter a date for this entry'
+    //                     value={entry.date}
+    //                     onChange={handleChange}
+    //                 />
+    //                 <input
+    //                     name="image"
+    //                     placeholder='Image'
+    //                     value={entry.imageURL}
+    //                     onChange={handleChange}
+    //                 />
+    //                 <input
+    //                     name="content"
+    //                     placeholder="Dear diary... "
+    //                     value={entry.content}
+    //                     onChange={handleChange}
+    //                 />
+    //             </div>
+    //         )
+    //     })}
+    //  </form>
+    // </div>
+    )
 }
 
-export default newEntry;
+export default AddEntry;
